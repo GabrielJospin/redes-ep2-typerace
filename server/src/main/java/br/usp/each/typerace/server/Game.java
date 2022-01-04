3cfb1aa676aeb0673cc14b65b8c6f98747d6425a
gabij <gabi.jospin@gmail.com>
2022-01-04 15:29:48 -0300

    Creat class Game

---
+++ server/src/main/java/br/usp/each/typerace/server/Game.java
@@ -0,0 +1,51 @@
+package br.usp.each.typerace.server;
+
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class Game {
+
+    private static final String PATH_TO_SETTINGS = "./src/main/resources/serverSettings.json";
+
+    private final int maxScore;
+    private final int maxPlayer;
+    private final int numberOfWords;
+    private final Map<String, Player> scoreBoard;
+    private boolean isGameFinished;
+    private long timeInit;
+    private long timeEnd;
+
+    public Game(Map<String, Player> scoreBoard, boolean isGameFinished, long timeInit, long timeEnd) {
+
+        JSONObject json = getJson(PATH_TO_SETTINGS);
+        this.maxScore = json.getInt("maxScore");
+        this.maxPlayer = json.getInt("maxPlayer");
+        this.numberOfWords = json.getInt("numberOfWords");
+        this.scoreBoard = scoreBoard;
+        this.isGameFinished = isGameFinished;
+        this.timeInit = timeInit;
+        this.timeEnd = timeEnd;
+    }
+
+    @Contract("_ -> new")
+    public static @NotNull JSONObject getJson(String path){
+        File file = new File(path);
+        String content = "";
+
+        try {
+            content = FileUtils.readFileToString(file,"utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new JSONObject(content);
+    }
+
+
+}
