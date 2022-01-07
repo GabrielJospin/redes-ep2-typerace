e20d3cca85d0f60c7e803f2c1c480fb2a97904ee
gabij <gabi.jospin@gmail.com>
2022-01-07 16:08:10 -0300

    correct some bugs or something like that

--- client/src/main/java/br/usp/each/typerace/client/ClientMain.java
+++ client/src/main/java/br/usp/each/typerace/client/ClientMain.java
@@ -9,9 +9,7 @@ import java.util.Scanner;
 public class ClientMain {
 
     private WebSocketClient client;
-    private final Scanner scanner = new Scanner(System.in);
-    private int port;
-    private String id;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public ClientMain(WebSocketClient client) {
         this.client = client;
@@ -22,26 +20,28 @@ public class ClientMain {
         client.connect();
     }
 
-    private void configPort(){
+    private static int configPort(){
         System.out.println("We need tell us, what port you want connect, for default we use the port 8080");
+        System.out.println();
         String in = scanner.nextLine();
-        int temp = Integer.getInteger(in);
-        if(0 < temp && temp < 65535)
-            this.port = temp;
-        else{
-            System.out.println("Wrong port try again");
-            configPort();
+        int temp = Integer.parseInt(in);
+        if(0 < temp && temp < 65535) {
+            System.out.println(temp);
+            return temp;
         }
+        System.out.println("Wrong port try again");
+        return configPort();
+
 
     }
 
-    private void configID(){
+    private static String configID(){
         System.out.println("Now we need choice your ID (Without space, please), choice a nice name");
         String id = scanner.next();
         System.out.println();
         if(id.isBlank() || id.equalsIgnoreCase("DanielCordeiro")){
             System.out.println("ARE U KIDDING ME?? TRY AGAIN, PLEASE");
-            configID();
+            return configID();
         }else{
             if(id.equalsIgnoreCase("ActuallyAReallyNiceId"))
                 System.out.println("OMG THAT'S ID IS VERY NICE");
@@ -49,11 +49,11 @@ public class ClientMain {
                 System.out.println("Your id is AMAZING");
             else
                 System.out.println("You think that's nice\n... Okay, if you think\n... Let do it");
-            this.id = id;
+            return id;
         }
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         /*
            FIXME: Remover essas strings fixas
            //POR QUE??? ELAS TEM SENTIMENTOS SABIA!!!
@@ -62,9 +62,9 @@ public class ClientMain {
            ele deve se conectar e o seu ID?
         */
         System.out.println("Welcome to INFINITE MONKEY's game");
-        configPort();
-        configID();
-        String portURL = "ws://localhost:"+ getPort() +"/playerID="+getId();
+        int port =  configPort();
+        String id = configID();
+        String portURL = "ws://localhost:"+ port +"/playerID="+id;
 
         try {
             WebSocketClient client = new Client(new URI(portURL));
@@ -77,11 +77,4 @@ public class ClientMain {
         }
     }
 
-    public int getPort() {
-        return port;
-    }
-
-    public String getId() {
-        return id;
-    }
 }
